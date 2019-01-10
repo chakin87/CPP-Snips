e46c0bf6fe524fcdac9817b83514ae3abb8a30bc
chakin87 <christopherthamm@yahoo.com>
2019-01-09 23:47:41 -0600

    Started, but not ready

---
+++ casting.cpp
@@ -0,0 +1,28 @@
+//				How to cast fly fishing!
+//   Actually, that's not right... Casting types in C++!!!!
+// So, here are the different types of casting in C/C++
+//	  C++ style casts
+// A) static_cast<T>
+// B) reinterpret_cast<T>()
+// C) dynamic_cast<T>()
+// D) const_cast<T>()
+//    C style casts
+// E) implicit
+// F) (int), (float), (char), (int*), etc
+// D) 
+// E) 
+// F) 
+
+
+
+#include "functions.h"
+
+
+
+int main(int argc, char** argv) {
+
+
+
+	std::cin.get();
+	return 0;
+}
