64bf9cc4a7209192855f9e67e8ab051033149671
chakin87 <christopherthamm@yahoo.com>
2019-02-24 00:08:56 -0600

    Moved Code into function.

--- func-Math-QuadraticRoots.cpp
+++ func-Math-QuadraticRoots.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
-int main() {
+void FindQuadraticRoot(float a, float b, float c) {
+	float x1, x2, discriminant, realPart, imaginaryPart;
 
-	float a, b, c, x1, x2, discriminant, realPart, imaginaryPart;
-	cout << "Enter coefficients a, b and c: ";
-	cin >> a >> b >> c;
 	discriminant = b * b - 4 * a*c;
 
 	if (discriminant > 0) {
 		x1 = (-b + sqrt(discriminant)) / (2 * a);
 		x2 = (-b - sqrt(discriminant)) / (2 * a);
-		cout << "Roots are real and different." << endl;
-		cout << "x1 = " << x1 << endl;
-		cout << "x2 = " << x2 << endl;
+		std::cout << "Roots are real and different." << std::endl;
+		std::cout << "x1 = " << x1 << std::endl;
+		std::cout << "x2 = " << x2 << std::endl;
 	}
 
 	else if (discriminant == 0) {
-		cout << "Roots are real and same." << endl;
+		std::cout << "Roots are real and same." << std::endl;
 		x1 = (-b + sqrt(discriminant)) / (2 * a);
-		cout << "x1 = x2 =" << x1 << endl;
+		std::cout << "x1 = x2 =" << x1 << std::endl;
 	}
 
 	else {
 		realPart = -b / (2 * a);
 		imaginaryPart = sqrt(-discriminant) / (2 * a);
-		cout << "Roots are complex and different." << endl;
-		cout << "x1 = " << realPart << "+" << imaginaryPart << "i" << endl;
-		cout << "x2 = " << realPart << "-" << imaginaryPart << "i" << endl;
+		std::cout << "Roots are complex and different." << std::endl;
+		std::cout << "x1 = " << realPart << "+" << imaginaryPart << "i" << std::endl;
+		std::cout << "x2 = " << realPart << "-" << imaginaryPart << "i" << std::endl;
 	}
+}
+int main() {
+
+	float a, b, c, x1, x2, discriminant, realPart, imaginaryPart;
+	std::cout << "Enter coefficients a, b and c: ";
+	std::cin >> a >> b >> c;
+
+	FindQuadraticRoot(a, b, c);
 
 	int pause;
 	std::cin >> pause;
